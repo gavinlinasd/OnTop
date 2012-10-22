719cac16c78f266deaec57ae16ba531d0de2ddeb
Gavin <user@ChrUbuntu.(none)>
2012-10-21 20:08:53 -0400

    Create the dummy rails application. Fix a typo in README file. (For some reason I have to install nodejs to succesfully run 'rails server', I am not sure if that's the case for other computer.

---
+++ OnTop/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnTop::Application.config.secret_token = 'dbf5c3ea6e94750f51ddb9c40f7fa35a3001bf72f2a83a46782646e0178ec1bd8da5c127c78fd609483741a9c2586f094492169be6c451a6abd9ea829eac6e8d'
