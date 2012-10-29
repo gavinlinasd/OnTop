980219e3123c2f16dbf1287d49a2add39af0fb62
Gavin <gavin.lin.asd@gmail.com>
2012-10-28 22:17:12 -0400

    Added extra actions in search controller and its corresponding route in routes.rb

--- OnTop/app/controllers/search_controller.rb
+++ OnTop/app/controllers/search_controller.rb
@@ -6,4 +6,34 @@ class SearchController < ApplicationController
   def show
   	@key_word = params[:keyword]
   end
+
+  ############################################
+  # The following will be functional actions #
+  # and has no corresponding view component	 #
+  ############################################
+
+  def fetch_by_keyword
+	# Fetch related keywords by the keyword
+
+  	@keyword = params[:keyword]
+
+	if @keyword==nil
+		@result = Keyword.all
+	else
+		@result = Keyword.find_by_name(@keyword)
+	end
+
+	# Return in the format of JSON object
+	respond_to do |format|
+		format.json { render :json => @result }
+	end
+
+  end
+
+  def fetch_by_keyword_pairs
+  end
+
+  def fetch_info_by_keyword
+  end
+
 end
