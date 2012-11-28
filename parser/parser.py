ecddc21d3bfc93dedb5e3fdf6e86af8969467b90
Harsimran Singh <manug@umich.edu>
2012-11-28 00:10:50 -0500

    first version of parser.py

--- parser/parser.py
+++ parser/parser.py
@@ -1,16 +1,26 @@
 import xml.etree.cElementTree as ET
+import sqlite3 as lite
+import datetime
+import time
+import sys
 
 filename='snippet.xml'
 prefix='{http://www.mediawiki.org/xml/export-0.8/}'
 
-titles=[]
+con = lite.connect('development.sqlite3')
+
+cur = con.cursor()
 
 for event, elem in ET.iterparse(filename):
 	if elem.tag == prefix+"title":
-		titles.append(elem.text)
+		now = datetime.datetime.now()
+		sql ='INSERT INTO "keywords" ("created_at", "name", "updated_at", "wiki_page") VALUES (%s, %s, %s, %s)' % (time.mktime(now.timetuple()), '"'+elem.text+'"', time.mktime(now.timetuple()), '"randomepage"')
+		print sql
+		cur.execute(sql)
 	elem.clear()
 
-print titles
-print len(titles)
+print "Done updating dB"
 
-print "Done parsing"
+con.commit();
+con.close();
+
