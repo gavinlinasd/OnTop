5633111105214479378b8314a98344ed3d869dec
Harsimran Singh <manug@umich.edu>
2012-12-01 00:38:34 -0500

    Add feedback ticket system

--- OnTop/db/schema.rb
+++ OnTop/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121201042554) do
+ActiveRecord::Schema.define(:version => 20121201051657) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -47,7 +47,7 @@ ActiveRecord::Schema.define(:version => 20121201042554) do
   create_table "tickets", :force => true do |t|
     t.string   "name"
     t.string   "email"
-    t.text     "subject"
+    t.string   "subject"
     t.text     "body"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
